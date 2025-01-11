89482482f8b66fd3051aa2016844da3e523a37c8
ZakBMorgan <114784064+ZakBMorgan@users.noreply.github.com>
2025-01-11 15:00:04 -0800

    basic subsystem and command
    added deephang subsystem and teleop command

---
+++ 2025-ReefScape/src/main/java/frc/robot/subsystems/swerve/DeepHang.java
@@ -0,0 +1,72 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.swerve;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkLimitSwitch;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DeepHang extends SubsystemBase {
+  /** Creates a new DeepHang. */
+  SparkMax hangSparkMax;
+
+  public SparkLimitSwitch upperLimit;
+  public SparkLimitSwitch lowerLimit;
+
+  boolean override = false;
+
+  RelativeEncoder encoder;
+
+  public DeepHang() {
+
+    SparkMaxConfig config = new SparkMaxConfig();
+    hangSparkMax.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    hangSparkMax = new SparkMax(0, MotorType.kBrushless); //deviceID = 0?
+
+    upperLimit = hangSparkMax.getForwardLimitSwitch();
+    lowerLimit = hangSparkMax.getReverseLimitSwitch();
+
+    encoder = hangSparkMax.getEncoder();
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    if (upperLimit.isPressed()) {
+      hangSparkMax.set(0);
+      override = true;
+    }
+    
+    if (lowerLimit.isPressed()) {
+      hangSparkMax.set(0);
+      override = true;
+    }
+
+    SmartDashboard.putBoolean("upper Limit", upperLimit.isPressed());
+    SmartDashboard.putBoolean("lower Limit", lowerLimit.isPressed());
+    SmartDashboard.putNumber("encoder position", encoder.getPosition());
+  }
+
+  public void resetEncoder() {
+    encoder.setPosition(0);
+  }
+
+  public void set(double speed) {
+    if (!override) {
+      hangSparkMax.set(speed);
+    } else {
+      hangSparkMax.set(0);
+    }
+  }
+}
