8c1baae812d214405af6728ec404005df799d651
ZakBMorgan <114784064+ZakBMorgan@users.noreply.github.com>
2025-01-11 15:23:55 -0800

    updated deephang file location
    moved deephang to general command folder and general subsystem folder

--- 2025-ReefScape/src/main/java/frc/robot/commands/DeepHangTeleop.java
+++ 2025-ReefScape/src/main/java/frc/robot/commands/DeepHangTeleop.java
@@ -2,11 +2,11 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.swerve;
+package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.swerve.DeepHang;
+import frc.robot.subsystems.DeepHang;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class DeepHangTeleop extends Command {
