3651db929add12870d1cd9ae9c181d2cc21e73d3
ZakBMorgan <114784064+ZakBMorgan@users.noreply.github.com>
2025-01-11 15:25:35 -0800

    added code to deephangteleop

--- 2025-ReefScape/src/main/java/frc/robot/commands/DeepHangTeleop.java
+++ 2025-ReefScape/src/main/java/frc/robot/commands/DeepHangTeleop.java
@@ -45,7 +45,9 @@ public class DeepHangTeleop extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    hang.set(0);
+  }
 
   // Returns true when the command should end.
   @Override
