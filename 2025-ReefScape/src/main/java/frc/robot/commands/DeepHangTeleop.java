1ac246ab7db4b2d9553fb3bb296d515b8fab6745
ZakBMorgan <114784064+ZakBMorgan@users.noreply.github.com>
2025-01-11 16:33:18 -0800

    updated buttons
    renamed button variable from "up" to "hat" and added to SmartDashboard

--- 2025-ReefScape/src/main/java/frc/robot/commands/DeepHangTeleop.java
+++ 2025-ReefScape/src/main/java/frc/robot/commands/DeepHangTeleop.java
@@ -5,6 +5,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.DeepHang;
 
@@ -31,16 +32,19 @@ public class DeepHangTeleop extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double up = joystick.getPOV();
-    if(up < 15 || up > 345) {
+    double hat = joystick.getPOV();
+
+    if(hat < 15 || hat > 345) {
       hang.set(0.3);
     } 
-    if( up < 195 && up > 165) {
+    if(hat < 195 && hat > 165) {
       hang.set(-0.3);
     }
     else {
       hang.set(0);
     }
+
+    SmartDashboard.putNumber("hat degree", hat);
   }
 
   // Called once the command ends or is interrupted.
