c3fcf68076cec5bfdc3860f5740e01de26b8df94
ZakBMorgan <114784064+ZakBMorgan@users.noreply.github.com>
2025-01-31 20:29:45 -0800

    removed pid

--- 2025-ReefScape/src/main/java/frc/robot/subsystems/DeepHang.java
+++ 2025-ReefScape/src/main/java/frc/robot/subsystems/DeepHang.java
@@ -5,10 +5,8 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLimitSwitch;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
@@ -20,24 +18,23 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.ModuleConstants;
 
 public class DeepHang extends SubsystemBase {
   /** Creates a new DeepHang. */
   SparkMax deepHang;
 
-  private double hangkP = 0.1; //initial test value
-  private double hangkI = 0.0;
-  private double hangkD = 0.0;
+  // private double hangkP = 0.1; //initial test value
+  // private double hangkI = 0.0;
+  // private double hangkD = 0.0;
 
-  private double hangkF = 0.0;
+  // private double hangkF = 0.0;
 
   public SparkLimitSwitch upperLimit;
   public SparkLimitSwitch lowerLimit;
 
-  public SparkClosedLoopController hangPID;
+  //public SparkClosedLoopController hangPID;
 
-  public DigitalInput sensor;
+  public DigitalInput input;
   
   //2 limit switches
   //1 induction sensor -- 
@@ -50,26 +47,26 @@ public class DeepHang extends SubsystemBase {
   public DeepHang() {
 
     deepHang = new SparkMax(22, MotorType.kBrushless); //CANID = 22
-    hangPID = deepHang.getClosedLoopController();
     hangEncoder = deepHang.getEncoder();
+    //hangPID = deepHang.getClosedLoopController();
 
     SparkMaxConfig config = new SparkMaxConfig();
 
     config.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder);
 
     // Set conversion factors
-    config.encoder.positionConversionFactor(ModuleConstants.drivingEncoderPositionFactor);
-    config.encoder.velocityConversionFactor(ModuleConstants.drivingEncoderVelocityPositionFactor);
+    //config.encoder.positionConversionFactor(ModuleConstants.drivingEncoderPositionFactor);
+    //config.encoder.velocityConversionFactor(ModuleConstants.drivingEncoderVelocityPositionFactor);
   
-    config.closedLoop.pidf(hangkP, hangkI, hangkD, hangkF);
-    config.closedLoop.outputRange(-1, 1);
+    //config.closedLoop.pidf(hangkP, hangkI, hangkD, hangkF);
+    //config.closedLoop.outputRange(-1, 1);
 
     config.idleMode(IdleMode.kBrake);
 
     upperLimit = deepHang.getForwardLimitSwitch();
     lowerLimit = deepHang.getReverseLimitSwitch();
 
-    sensor = new DigitalInput(0); //proximity sensor
+    input = new DigitalInput(0); //proximity sensor
 
     deepHang.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
@@ -85,27 +82,12 @@ public class DeepHang extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    
-    // if (upperLimit.isPressed()) {
-    //   deepHang.set(0);  
-    // }
-    
-    // if (lowerLimit.isPressed()) {
-    //   deepHang.set(0);
-    // }
 
-    // SmartDashboard.putBoolean("upper Limit", upperLimit.isPressed());
-    // SmartDashboard.putBoolean("lower Limit", lowerLimit.isPressed());
-    // SmartDashboard.putBoolean("proximity sensor", sensor.get());
-
-    SmartDashboard.putNumber("encoder speed", (int) getEncoderVelocity());
-    SmartDashboard.putNumber("encoder position", (int) getEncoderPosition());
-    SmartDashboard.putNumber("hang kP", hangkP);
-    SmartDashboard.putNumber("hang kI", hangkI);
-    SmartDashboard.putNumber("hang kD", hangkD);
-    SmartDashboard.putNumber("hang kF", hangkF);
+    SmartDashboard.putNumber("Encoder Speed", (int) getEncoderVelocity());
+    SmartDashboard.putNumber("Encoder Position", (int) getEncoderPosition());
     SmartDashboard.putNumber("Encoder Position", getEncoderPosition());
     SmartDashboard.putNumber("Encoder Velocity", getEncoderVelocity());
+    SmartDashboard.putBoolean("Proximity Sensor", input.get());
 
   }
 
@@ -114,8 +96,7 @@ public class DeepHang extends SubsystemBase {
   }
 
   public void setSpeed(double setPoint) {
-    hangPID.setReference(setPoint, ControlType.kVelocity);
-    // deepHang.set(setPoint);
+    deepHang.set(setPoint);
   }
 
 
